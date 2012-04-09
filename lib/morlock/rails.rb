1897bc6765f964ca706a82b50a839b945716ca16
Chris Alvarado-Dryden & Matthew LaRocca <pair+chris+matthew@mavenlink.com>
2012-04-09 11:06:17 -0700

    fixing dalli detection in rails

--- lib/morlock/rails.rb
+++ lib/morlock/rails.rb
@@ -3,8 +3,8 @@ require 'morlock/base'
 
 class Morlock
   class MorlockRailtie < ::Rails::Railtie
-    config.after_initialize do
-      if defined?(ActiveSupport::Cache::MemCacheStore) && Rails.cache.is_a?(ActiveSupport::Cache::MemCacheStore) && Rails.cache.instance_variable_get(:@data)
+    def self.setup_for_mem_cache_store
+      if Rails.cache.instance_variable_get(:@data)
         Rails.module_eval do
           class << self
             def morlock
@@ -12,9 +12,26 @@ class Morlock
             end
           end
         end
+      end
+    end
+
+    def self.setup_for_dalli_store
+      self.setup_for_mem_cache_store
+    end
+
+    def self.detect_memcache_gem
+      if defined?(ActiveSupport::Cache::MemCacheStore) && Rails.cache.is_a?(ActiveSupport::Cache::MemCacheStore)
+        setup_for_mem_cache_store
+      elsif defined?(ActiveSupport::Cache::DalliStore) && Rails.cache.is_a?(ActiveSupport::Cache::DalliStore)
+        setup_for_dalli_store
       else
         Rails.logger.warn "WARNING: Morlock detected that you are not using the Rails ActiveSupport::Cache::MemCacheStore.  Rails.morlock will not be setup."
       end
+
+    end
+
+    config.after_initialize do
+      detect_memcache_gem
     end
   end
 end
