e8a5e45cdb1e4ac00104451f3d3b4f07b86958b2
Andrew Cantino <cantino@gmail.com>
2012-11-07 18:00:33 -0800

    add logging

--- lib/morlock/base.rb
+++ lib/morlock/base.rb
@@ -11,11 +11,18 @@ class Morlock
 
   def lock(key, options = {})
     lock_obtained = @client.add(key, options[:expiration] || DEFAULT_EXPIRATION)
+    puts "Lock for #{key} #{lock_obtained ? "obtained" : "not obtained"}." if options[:verbose]
     yield if lock_obtained && block_given?
     options[:success].call if lock_obtained && options[:success]
     options[:failure].call if !lock_obtained && options[:failure]
     lock_obtained
   ensure
-    @client.delete(key) if lock_obtained
+    if lock_obtained
+      if @client.delete(key)
+        puts "Lock removed for #{key}" if options[:verbose]
+      else
+        puts "Someone else removed the lock for #{key}!" if options[:verbose]
+      end
+    end
   end
 end
